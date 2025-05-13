816be6c61cd9dae35ff7aa2d147eff5542fe8066
Alexis Morales <alexis.ivan.mt@gmail.com>
2025-05-12 21:54:52 -0600

    Refactor(server): separate routes from server logic

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 // index.js
-// where your node app starts
+// Inicializa el servidor
 
-// init project
-var express = require('express');
-var app = express();
+var app = require('./myapp'); // Importa la lógica de la aplicación
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// Listen on port set in environment variable or default to 3000
+// Escucha en el puerto configurado
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
